64b4d271882e1e3060b64edc704bdd677ee5ab37
NikolayK <nikolay.kim@codeimmersive.com>
2019-11-27 12:46:28 -0500

    adds solution

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
-const Stack = function() {
+const Stack = function(items) {
+  return {
+    items: [],
+
+    add: function(item){
+      this.items.push(item)
+      return this.items
+    },
+
+    remove: function(){
+      return this.items.splice(this.items.length-1,1)[0]
+    },
+
+    peek: function(){
+      return this.items[this.items.length-1]
+    }
+  }
 }
 
 const Queue = function() {
+  return {
+    items: [],
+
+    add: function(item){
+      this.items.push(item)
+      return this.items
+    },
+
+    remove: function(){
+      return this.items.splice(0,1)[0]
+    },
+
+    peek: function(){
+      return this.items[0]
+    }
+  }
 }
 
 module.exports = {
